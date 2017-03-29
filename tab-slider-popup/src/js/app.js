a2378c2b93ac16aa133f7754c0af936155b33bdf
BaQudo <baqudo@gmail.com>
2017-03-29 12:55:45 +0300

    upd (now work with more then one tabs)

--- tab-slider-popup/src/js/app.js
+++ tab-slider-popup/src/js/app.js
@@ -8,85 +8,93 @@ sayHello();
 
 // --------------------------------------tab-slider-popup-slider--------------------------------------
 $(function () {
+  var 
+    $tabs = $('.tabs'),
+    slickOpt = {
+      infinite: false,
+      slidesToShow: 2,
+      slidesToScroll: 1
+    };
 
-  var slickOpt = {
-    infinite: false,
-    slidesToShow: 2,
-    slidesToScroll: 1
-  };
-
-  var $tabControl = $('.tabs__link'),
-      $tabControlNotActive = $tabControl.filter(':not(.is-active)'),
-      $contentList = $('.tabs__list'),
-      $contentItem = $('.tabs__content'),
-      $sliders = $('.slider'),
-      $moreBtn = $('.js-more-btn'),
-      $closeBtn = $('.popup-close-btn'),
-      $popup = $('.popup-container'),
-      $popupBox = $('.popup-box'),
-      $popupSlider = $('.popup-slider');
-
-    $('.slider').filter('.is-active').slick(slickOpt);
+  $('.slider').filter('.is-active').slick(slickOpt)
 
 
-  function initSlider(e){
+  $tabs.each(function (){
+    var 
+        $tabNav = $(this).find('.tabs__nav'),
+        $tabControl = $(this).find('.tabs__link'),
+        $tabControlNotActive = $tabControl.filter(':not(.is-active)'),
+        $contentList = $(this).find('.tabs__list'),
+        $contentItem = $(this).find('.tabs__content'),
+        $sliders = $(this).find('.slider'),
+        $moreBtn = $(this).find('.js-more-btn'),
+        $closeBtn = $('.popup-close-btn'),
+        $popup = $('.popup-container'),
+        $popupBox = $('.popup-box'),
+        $popupSlider = $('.popup-slider');
 
-    var
-      $target = $(this),
-      itemPosition = $target.index();
+    function initSlider(e){
 
-    $target.addClass('is-active').siblings().removeClass('is-active');   
-    $contentItem.eq(itemPosition).addClass('is-active').siblings().removeClass('is-active');
+      var
+        $target = $(this),
+        itemPosition = $target.index();
 
-    if ($('.slider:hidden').hasClass('slick-slider')) {
-      $('.slider:hidden').slick('unslick');
-      console.log('msg')
+      $target.addClass('is-active').siblings().removeClass('is-active');   
+      $contentItem.eq(itemPosition).addClass('is-active').siblings().removeClass('is-active');
+
+      var $sliderHidden = $contentItem.find('.slider:hidden');
+
+      if ($sliderHidden.hasClass('slick-slider')) {
+        $sliderHidden.slick('unslick');
+        console.log('msg')
+      }
+
+      $contentItem.eq(itemPosition).children('.slider').slick(slickOpt);
     }
 
-    $contentItem.eq(itemPosition).children('.slider').slick(slickOpt);
-  }
+
+    function initPopup() {
+      var $this = $(this);
+      var newSlider = $this.parents('.slider');
+      var $currentSlide = $this.parent().index();
+      var newSlides = newSlider.find('.slide');
+      var $slideCounterCurrent = $('.popup-slide-counter__current');
+      var $slideCounterAll = $('.popup-slide-counter__all');
+
+      $popup.css('display', 'block');
+      $slideCounterCurrent.text($currentSlide + 1);
+      $slideCounterAll.text(newSlides.length);
+
+      newSlides.clone().appendTo($popupSlider);
+
+      $popupSlider.slick({
+        infinite: false,
+        arrows: true,
+        initialSlide: $currentSlide
+      });
+
+      $popupSlider.on('afterChange', function(event, slick, currentSlide) {
+        $slideCounterCurrent.text(currentSlide + 1);
+      });
+    }
+
+    function closePopup() {
+      $popup
+        .find('.popup-slider')
+        .slick('unslick')
+        .children().remove();
+      $popup.css('display', 'none');
+    }
+
+    $tabControl.on('click', initSlider);
+    $moreBtn.on('click', initPopup);
+    $closeBtn.on('click', closePopup);
 
 
-  function initPopup() {
-    var $this = $(this);
-    var newSlider = $this.parents('.slider');
-    var $currentSlide = $this.parent().index();
-    var newSlides = newSlider.find('.slide');
-    var $slideCounterCurrent = $('.popup-slide-counter__current');
-    var $slideCounterAll = $('.popup-slide-counter__all');
-
-    $popup.css('display', 'block');
-    $slideCounterCurrent.text($currentSlide + 1);
-    $slideCounterAll.text(newSlides.length);
-
-    newSlides.clone().appendTo($popupSlider);
-
-    $popupSlider.slick({
-      infinite: false,
-      arrows: true,
-      initialSlide: $currentSlide
+    $popup.on('click', function(event) {
+      if (event.target == this) closePopup();
     });
 
-    $popupSlider.on('afterChange', function(event, slick, currentSlide) {
-      $slideCounterCurrent.text(currentSlide + 1);
-    });
-  }
-
-  function closePopup() {
-    $popup
-      .find('.popup-slider')
-      .slick('unslick')
-      .children().remove();
-    $popup.css('display', 'none');
-  }
-
-  $tabControl.on('click', initSlider);
-  $moreBtn.on('click', initPopup);
-  $closeBtn.on('click', closePopup);
-
-
-  $popup.on('click', function(event) {
-    if (event.target == this) closePopup();
   });
 });
 
